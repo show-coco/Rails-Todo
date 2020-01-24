799fcd69ae97ab21a53b8e28889e03201f3094ac
show <show75@ezweb.ne.jp>
2020-01-24 17:44:08 +0900

    #1 デバッグ用コードを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(params[:users])
     
     if @user.save
       # リダイレクト
