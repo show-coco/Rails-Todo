c3c6abb0dffc02008301cf266db6e1a9cd0094ed
show <show75@ezweb.ne.jp>
2020-01-26 11:28:29 +0900

    #1 ルーティングエラーを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/users', to: 'users#new'
   resources 'users', only: ['show', 'new', 'create', 'destroy']
 end
