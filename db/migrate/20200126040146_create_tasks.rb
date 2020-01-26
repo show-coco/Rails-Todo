13b4e990140e8e1f0a0b758b32ead5b8b9fd214b
show <show75@ezweb.ne.jp>
2020-01-26 13:03:38 +0900

    #1 Taskモデルを作成

--- db/migrate/20200126040146_create_tasks.rb
+++ db/migrate/20200126040146_create_tasks.rb
@@ -3,8 +3,10 @@ class CreateTasks < ActiveRecord::Migration[6.0]
     create_table :tasks do |t|
       t.string :title
       t.integer :status
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_index :tasks, [:user_id, :created_at]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_26_012959) do
+ActiveRecord::Schema.define(version: 2020_01_26_040146) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.integer "status"
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_tasks_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +31,5 @@ ActiveRecord::Schema.define(version: 2020_01_26_012959) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
