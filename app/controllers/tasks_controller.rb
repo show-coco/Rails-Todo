14f138c4c521bc63de5ad12dbf2885c17ee2e8ca
show <show75@ezweb.ne.jp>
2020-01-27 19:35:37 +0900

    #1 タスク管理画面のレイアウト変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,19 @@ class TasksController < ApplicationController
 
   def index
     @user = User.find(params[:id])
+    @tasks = Task.all.where(user_id: @user.id)
+    @task = Task.new
+  end
+
+  def create
+    @task  = current_user.tasks.build(tasks_params)
+    @task.status = params[:id]
+    if @task.save
+      flash[:success] = "タスクを追加しました"
+      redirect_to tasks_path(current_user)
+    else
+      render 'index'
+    end
   end
 
   private 
@@ -10,5 +23,9 @@ class TasksController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to tasks_path(current_user) unless @user == current_user
+    end
+
+    def tasks_params
+      params.require(:task).permit(:title, :status)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'sessions#new'
   get 'tasks/:id', to: 'tasks#index', as: 'tasks'
+  post 'tasks/:id', to: 'tasks#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
